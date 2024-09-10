69908cf649a6fd36024da1e1be6ad8f84ff0a6ff
Ish Kumar <help.arinsharma+git@gmail.com>
2024-09-10 23:43:42 +0530

    navbar 2 commit

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
-  return(
-    <div className="flex justify-center items-center top-3 fixed w-full z-10">
+  return (
+    <div className="flex justify-center sm:justify-start items-center top-3 left-8 fixed w-full z-10">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
         <a className="nav-item" href="/">Home</a>
         <a className="nav-item" href="/">Projects</a>
